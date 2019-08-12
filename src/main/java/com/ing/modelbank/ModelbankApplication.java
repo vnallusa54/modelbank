654d791d76d9937601799cb7d4482f1823758276
Alphathon <53964655+Alphathon@users.noreply.github.com>
2019-08-12 13:06:20 +0530

    initial commit

---
+++ src/main/java/com/ing/modelbank/ModelbankApplication.java
@@ -0,0 +1,13 @@
+package com.ing.modelbank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ModelbankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ModelbankApplication.class, args);
+	}
+
+}
